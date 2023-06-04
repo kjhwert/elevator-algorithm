5c1a15ee5dc20f821b138bb4123cfa9b076649a0
kjhwert <kjhwert11@naver.com>
2023-06-04 21:57:17 +0900

    refactor: 2번 문제 풀기 전 구조 변경

---
+++ problems/elevator/answer1.ts
@@ -0,0 +1,25 @@
+export interface Elevator {
+  id: number;
+  floor: number;
+}
+
+const answer1 = (guestFloor: number, elevators: Elevator[]): Elevator => {
+  const [minDistanceElevator] = elevators
+    .map((elevator) => ({
+      ...elevator,
+      distance: Math.abs(guestFloor - elevator.floor),
+    }))
+    .sort((a, b) => {
+      if (a.distance === b.distance) {
+        return b.floor - a.floor;
+      }
+
+      return a.distance - b.distance;
+    });
+
+  const { distance, ...elevator } = minDistanceElevator;
+
+  return elevator;
+};
+
+export default answer1;
