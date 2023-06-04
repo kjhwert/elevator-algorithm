ae40470eb65c8ddd742abe82e979d4e54d795e08
kjhwert <kjhwert11@naver.com>
2023-06-04 21:45:52 +0900

    feat: 엘리베이터 문제1 답안 작성

--- problems/elevator/elevator.ts
+++ problems/elevator/elevator.ts
@@ -1,5 +1,25 @@
-const answer1 = (elevatorFloors: number[]): number => {
-  return 0;
+export interface Elevator {
+  id: number;
+  floor: number;
+}
+
+const answer1 = (guestFloor: number, elevators: Elevator[]): Elevator => {
+  const [minDistanceElevator] = elevators
+    .map((elevator) => ({
+      ...elevator,
+      distance: Math.abs(guestFloor - elevator.floor),
+    }))
+    .sort((a, b) => {
+      if (a.distance === b.distance) {
+        return b.floor - a.floor;
+      }
+
+      return a.distance - b.distance;
+    });
+
+  const { distance, ...elevator } = minDistanceElevator;
+
+  return elevator;
 };
 
 const elevator = {
