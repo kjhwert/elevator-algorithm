8a454ba00060fd969295b29fe595477b28ee1854
kjhwert <kjhwert11@naver.com>
2023-06-07 21:55:32 +0900

    log: 코드 주석

--- problems/elevator/answer1.ts
+++ problems/elevator/answer1.ts
@@ -10,6 +10,7 @@ const answer1 = (guestFloor: number, elevators: Elevator[]): Elevator => {
       distance: Math.abs(guestFloor - elevator.floor),
     }))
     .sort((a, b) => {
+      // 엘리베이터의 거리가 동일한 경우 위에 있는 엘리베이터 호출
       if (a.distance === b.distance) {
         return b.floor - a.floor;
       }
